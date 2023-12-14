49a9f3047e091b85c8f999bdbde9719cf6dcba35
sami pevekar <sami@p.com>
2023-12-14 16:21:58 +0530

    final commit

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express=require("express")
 const app=express()
-const port =process.env.PORT || 80
+const cors=require("cors")
+
+const api=require("./data.json")
 
 
-const apiData=require("./data.json")
+const port=80
+app.use(cors())
+
+
 
 app.get("/",(req,res)=>{
     res.send("sami pevekar")
 })
 
 app.get("/service",(req,res)=>{
-    res.send(apiData)
+    res.send(api)
+
 })
 
+
 app.listen(port,()=>{
-    console.log("server is listeing on port ",port)
+    console.log(`server is listening on ${port}`)
 })
